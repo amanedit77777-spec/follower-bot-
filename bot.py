fac9649842c5e2cb6de7925e3540bdf2b8a51dc1
amanedit77777-spec <aman.edit77777@gmail.com>
2026-05-08 12:18:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ MAX_RELOGIN_ATTEMPTS_PER_SITE = 2
 
 
 options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
